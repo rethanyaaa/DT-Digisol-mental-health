2b9b8a1e71a6d677816b58aae43cb1c2038beae6
Rethanya <168968263+rethanyaaa@users.noreply.github.com>
2025-08-01 20:07:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ app.use(express.json({ limit: "400mb" }));
 // CORS configuration for both frontend origins
 const corsOptions = {
   origin: [
-    "https://dtdigisol-mental-health.netlify.app/",
-    "https://dtdigisol-admin.netlify.app/",
+    "https://dtdigisol-mental-health.netlify.app",
+    "https://dtdigisol-admin.netlify.app",
    
   ].filter(Boolean),
   credentials: true,
